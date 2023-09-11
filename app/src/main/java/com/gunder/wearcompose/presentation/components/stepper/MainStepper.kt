5ca247c26a767adda73b67b9e848d2414cd3aa6a
gunadermawan <gunadermawan.official@gmail.com>
2023-09-11 11:54:07 +0700

    add headphone icon

--- app/src/main/java/com/gunder/wearcompose/presentation/components/stepper/MainStepper.kt
+++ app/src/main/java/com/gunder/wearcompose/presentation/components/stepper/MainStepper.kt
@@ -2,7 +2,13 @@ package com.gunder.wearcompose.presentation.components.stepper
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
@@ -10,11 +16,15 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.wear.compose.material.Card
 import androidx.wear.compose.material.Icon
 import androidx.wear.compose.material.InlineSliderDefaults
 import androidx.wear.compose.material.Stepper
 import androidx.wear.compose.material.Text
+import com.gunder.wearcompose.R
 
 @Composable
 fun MainStepper(modifier: Modifier = Modifier) {
@@ -42,7 +52,27 @@ fun MainStepper(modifier: Modifier = Modifier) {
                     contentDescription = null
                 )
             }) {
-            Text(text = "Volume Music : $value %")
+
+            Card(
+                onClick = { }, modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp),
+                shape = RoundedCornerShape(20.dp)
+            ) {
+                Row(
+                    modifier
+                        .fillMaxSize(),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Center
+                ) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_headphones),
+                        contentDescription = null
+                    )
+                    Spacer(modifier = Modifier.size(8.dp))
+                    Text(text = "Headphones $value %")
+                }
+            }
         }
     }
 }
